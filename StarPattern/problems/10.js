973b91a4e26d9ef9fa34baeb466c004bd19dea42
Azra <azraahmad316@gmail.com>
2025-08-22 15:41:40 +0530

    solved 10.js one ques left

--- StarPattern/problems/10.js
+++ StarPattern/problems/10.js
@@ -1,7 +1,7 @@
 //  Mixed Challenges
 /*
+1.
 
-Level 1
 *****
 ****
 ***
@@ -13,7 +13,23 @@ Level 1
 ****
 *****
 
-Level 2
+*/
+{
+    let n=5;
+    let row = 2*n;
+    for(let i=0; i<row; i++){
+        let star = "";
+        let count = (i<n)? n-i : i-n+1;
+        for(let j=0; j<count; j++){
+            star = star + "*";
+        }
+        console.log(star);
+    }
+}
+
+/*
+2.
+
 ****
 ***
 **
@@ -23,7 +39,44 @@ Level 2
 **
 *
 
-Level 3
+*/
+
+{
+    let n=4;
+    for(let i=0; i<n; i++){
+        let row = "";
+        for(let j=0; j<n-i; j++){
+            row = row + "*";
+        }
+        console.log(row);
+    }
+    for(let i=0; i<n; i++){
+        let row = "";
+        for(let j=0; j<n-i; j++){
+            row = row + "*";
+        }
+        console.log(row);
+    }
+
+}
+
+// potimize solution without DRY(don't repeat yourself)
+
+{
+    let n=4;
+    for(let i=0; i < 2*4; i++){
+        let row = "";
+        let star = (i<n) ? n-i : n-(i-n)
+        for(let j=0; j<star; j++){
+            row = row + "*";
+        }
+        console.log(row);
+    }
+}
+
+/*
+3.
+
 *******
 ******
 *****
@@ -39,7 +92,24 @@ Level 3
 ******
 *******
 
-Level 4
+*/
+
+{
+    let n=7;
+    for(let i=0; i<n*2; i++){
+        let row = "";
+        let star = (i<n) ? n-i : i-n+1;
+        for(let j=0; j<star; j++){
+            row = row + "*";
+        }
+        console.log(row);
+
+    }
+
+}
+/*
+4.
+
 *****
 *****
 ****
@@ -51,7 +121,14 @@ Level 4
 *
 *
 
-Level 5
+*/
+console.log("check")
+{
+
+}
+/*
+5.
+
 *
 **
 ***
@@ -63,3 +140,15 @@ Level 5
 *
 
 */
+
+{
+    let n=9;
+    for(let i=0; i<n; i++){
+        let row = "";
+        let star = (i<Math.ceil(n/2)) ? i+1 : n-i;
+        for(let j=0; j<star; j++){
+            row = row + "*";
+        }
+        console.log(row);
+    }
+}
