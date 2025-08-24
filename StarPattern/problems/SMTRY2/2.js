cf47db894aa994c7a42e2fe980f54b05ef61f1fb
Azra <azraahmad316@gmail.com>
2025-08-24 12:44:37 +0530

    symmetry with spaces 2.js solved

--- StarPattern/problems/SMTRY2/2.js
+++ StarPattern/problems/SMTRY2/2.js
@@ -1,8 +1,6 @@
+//Ladder 2 → Decreasing Symmetry (Inverted Pyramid)
+//Step 1 → Basics
 /*
-
-Ladder 2 → Decreasing Symmetry (Inverted Pyramid)
-Step 1 → Basics
-
 Q1:
 
 *****
@@ -11,103 +9,8 @@ Q1:
    **
     *
 
-
-Q2:
-
-*******
- *****
-  ***
-   *
-
-Step 2 → Different Heights
-
-Q3:
-
-******
- *****
-  ****
-   ***
-    **
-     *
-
-
-Q4:
-
-*********
- *******
-  *****
-   ***
-    *
-
-Step 3 → Wider Tops
-
-Q5:
-
-********
- *******
-  ******
-   *****
-    ****
-     ***
-      **
-       *
-
-
-Q6:
-
-***********
- *********
-  *******
-   *****
-    ***
-     *
-
-Step 4 → Pair Practice
-
-Q7:
-
-******
- *****
-  ****
-   ***
-    **
-     *
-
-
-Q8:
-
-**********
- ********
-  ******
-   ****
-    **
-
-Step 5 → Final Challenges
-
-Q9:
-
-***********
- *********
-  *******
-   *****
-    ***
-     *
-
-
-Q10:
-
-**************
- ************
-  **********
-   ********
-    ******
-     ****
-      **
-
 */
-
-//Q1:
-
+//1:
 {
      let n=5;
      for(let i=0; i<n; i++){
@@ -122,8 +25,16 @@ Q10:
           console.log(row);
      }
 }
+/*
+Q2:
 
-//Q2:
+*******
+ *****
+  ***
+   *
+
+*/
+//2:
 
 {
      let n=4;
@@ -140,8 +51,261 @@ Q10:
      }
 }
 
-//Q3:
-console.log("check");
+//Step 2 → Different Heights
+/*
+Q3:
+
+******
+ *****
+  ****
+   ***
+    **
+     *
+*/
+
+//3:
+
 {
-     
+     let n=6;
+     for(let i=0; i<n; i++){
+          let row = "";
+          //spaces
+          for(let j=0; j<i; j++){
+               row = row + " ";
+          }
+          //stars
+          for(let k=0; k< n-i; k++){
+               row = row + "*";
+          }
+          console.log(row)
+     }
 }
+
+/*
+Q4:
+
+*********
+ *******
+  *****
+   ***
+    *
+
+*/
+
+//4:
+
+{
+     let n=5;
+     for(let i=0; i<n; i++){
+          let row = "";
+          //spaces
+          for(let j=0; j<i; j++){
+          row= row + " ";
+          }
+          //stars
+          for(let k=0; k< 9-(2*i); k++){
+               row = row + "*";
+          }
+          console.log(row);
+          
+     }
+}
+
+//Step 3 → Wider Tops
+/*
+Q5:
+
+********
+ *******
+  ******
+   *****
+    ****
+     ***
+      **
+       *
+
+*/  
+//5:
+
+{
+     let n=8;
+     {
+          for(let i=0; i<n; i++){
+               let row = "";
+               //spaces
+               for(let j=0; j<i; j++){
+                    row = row + " ";
+               }
+               //stars
+               for(let k=0; k<n-i; k++){
+                    row = row + "*";
+               }
+               console.log(row);
+          }
+     }
+}
+
+/*
+Q6:
+
+***********
+ *********
+  *******
+   *****
+    ***
+     *
+
+*/
+//6:
+
+{
+     let n=6;
+     for(let i=0; i<n; i++){
+          let row = "";
+          //spaces
+          for(let j=0; j<i; j++){
+               row = row + " "
+          }
+          //stars
+          for(let k=0; k< 11-(2*i); k++){
+               row = row + "*";
+          }
+          console.log(row);
+     }
+}
+
+//Step 4 → Pair Practice
+/*
+Q7:
+
+******
+ *****
+  ****
+   ***
+    **
+     *
+
+*/
+//7:
+
+{
+     let n=6;
+     for(let i=0; i<n; i++){
+          let row = "";
+          //spaces
+          for(let j=0; j<i; j++){
+               row = row + " "
+          }
+          //stars
+          for(let k=0; k< n-i; k++){
+               row = row + "*";
+          }
+          console.log(row);
+     }
+}
+
+/*
+Q8:
+
+**********
+ ********
+  ******
+   ****
+    **
+
+*/
+//8:
+
+{
+     let n=5;
+     for(let i=0; i<n; i++){
+          let row = "";
+          //spaces
+          for(let j=0; j<i; j++){
+               row = row + " "
+          }
+          //stars
+          for(let k=0; k< 10-2*i; k++){
+               row = row + "*";
+          }
+          console.log(row);
+     }
+}
+
+//Step 5 → Final Challenges
+/*
+Q9:
+
+***********
+ *********
+  *******
+   *****
+    ***
+     *
+
+*/
+
+//9:
+
+{
+     let n=6;
+     for(let i=0; i<n; i++){
+          let row = "";
+          //spaces
+          for(let j=0; j<i; j++){
+               row = row + " "
+          }
+          //stars
+          for(let k=0; k< 11-2*i; k++){
+               row = row + "*";
+          }
+          console.log(row);
+     }
+}
+/*
+Q10:
+
+**************
+ ************
+  **********
+   ********
+    ******
+     ****
+      **
+
+*/
+
+//10:
+
+{
+     let n=7;
+     for(let i=0; i<n; i++){
+          let row = "";
+          //spaces
+          for(let j=0; j<i; j++){
+               row = row + " "
+          }
+          //stars
+          for(let k=0; k< 14-2*i; k++){
+               row = row + "*";
+          }
+          console.log(row);
+     }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
